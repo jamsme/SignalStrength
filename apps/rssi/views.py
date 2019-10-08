e2eb6e2cb856ff0d005022889287fe61637137f9
Juan Martinez <juanmartinezv6@gmail.com>
2019-10-07 22:56:34 -0700

    got the rssi of the laptop on a loop

--- apps/rssi/views.py
+++ apps/rssi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import subprocess
+import time
 
-# Create your views here.
+def index(request):
+    total = 1
+    stop = 0
+    num = 1
+    while(stop < total):
+        process = subprocess.Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport','-I'], stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        process.wait()
+
+        space = out.decode("utf-8").split()
+        time.sleep(2)
+        print(num," CtlRSSI = ",space[1])
+        num = num + 1
