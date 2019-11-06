61d276945665fd56d02ac60739c6aec38173cd67
Juan Martinez <juanmartinezv6@gmail.com>
2019-11-05 23:37:39 -0800

    changed the color based on the rssi

--- apps/rssi/static/rssi/js/jquery.js
+++ apps/rssi/static/rssi/js/jquery.js
@@ -2,14 +2,28 @@ $(document).ready(function () {
 
     $("button.waves-effect.waves-dark.btn-small").click(function() {
         console.log("button is clicked")
-        var i
         var limit = 5
-        for (i = 0; i < limit; i++) {
+        for (var i = 0; i < limit; i++) {
             $.ajax({
                 url: '/rssi',
-                success: function(resp){
-                    $("#hold").html(JSON.stringify(resp[1] + resp[2]))
-                    console.log(resp[1] + resp[2]);
+                success: function(resp) {
+                    $("#hold").html(JSON.stringify( resp + " dBm"))
+                    if (resp <= -69 && resp >= -67) {
+                        console.log("greater than 67 less than 69", resp)
+                        $(".card-panel").css('background-color', '#fcdd0f')
+                    }
+                    else if (resp <= -60 && resp >= -66) {
+                        console.log("greater than 60 less than 66", resp)
+                        $(".card-panel").css('background-color', '#f2ee1c')
+                    }
+                    else if (resp <= -50 && resp >= -59) {
+                        console.log("greater than 50 less than 59", resp)
+                        $(".card-panel").css('background-color', '#cce110')
+                    }
+                    else if (resp <= -0 && resp >= -49) {
+                        console.log("best", resp)
+                        $(".card-panel").css('background-color', '#7eca26')
+                    }
                 }
             })
         };
