0f95c585ffb254aa4792268b39c648454b7e039a
Juan Martinez <juanmartinezv6@gmail.com>
2019-11-21 23:19:20 -0800

    added back the bounce but it works now

--- apps/rssi/static/rssi/js/jquery.js
+++ apps/rssi/static/rssi/js/jquery.js
@@ -18,31 +18,37 @@ $(document).ready(function () {
                             console.log("best", resp, index)
                             $("#text").html("Perfect Signal");
                             $(".card-panel").css('background-color', '#00c853');
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                         else if (resp <= -50 && resp >= -59) {
-                            console.log(resp, "greater than 50 less than 59", index)
+                            console.log(resp, "greater than 50 less than 59", index);
                             $(".card-panel").css('background-color', '#43a047');
                             $("#text").html("Excellent Signal");
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                         else if (resp <= -60 && resp >= -66) {
                             console.log(resp, "greater than 60 less than 66", index)
                             $(".card-panel").css('background-color', '#66bb6a');
                             $("#text").html("Good, Reliable Signal");
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                         else if (resp <= -67 && resp >= -69) {
                             console.log(resp, "greater than 67 less than 69", index)
                             $(".card-panel").css('background-color', '#fcdd0f');
                             $("#text").html("Minimum for voice & non HD browsing");
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                         else if (resp <= -70 && resp >= -79) {
                             console.log(resp, "greater than 70 less than 79", index)
                             $(".card-panel").css('background-color', '#f8a62b');
                             $("#text").html("Light browsing and email only");
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                         else if (resp <= -80) {
                             console.log(resp, "bad", index)
                             $(".card-panel").css('background-color', '#ef4522');
                             $("#text").html("Horrible Connection");
+                            $(".card-panel").effect("bounce", { times: 3 }, "slow");
                         }
                     },
                     error: function() {
